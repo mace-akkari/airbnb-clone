450cc88db944ec953e9b25ab2e8cfd79330f7c60
Mace Akkari <mace.akkari@relyfe.com>
2023-05-02 12:09:07 +0100

    add complete login page

---
+++ client/src/Header.jsx
@@ -0,0 +1,82 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className="flex justify-between">
+      <a href="" className="flex items-center gap-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819"
+          />
+        </svg>
+        <span className="font-bold text-xl">Rent-A-Home </span>
+      </a>
+
+      <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300 ">
+        <div>Anywhere</div>
+        <div className="border-l border-gray-300"></div>
+        <div>Any week</div>
+        <div className="border-l border-gray-300"></div>
+        <div>Add guest</div>
+        <button className="bg-primary text-white p-1 rounded-full">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-4 h-4 "
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+          </svg>
+        </button>
+      </div>
+      <Link
+        to={"/login"}
+        className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </div>
+      </Link>
+    </header>
+  );
+}
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+export default function LoginPage() {
+  return (
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="mb-64">
+        <h1 className="text-3xl text-center mb-4">Login</h1>
+        <form className="max-w-md mx-auto">
+          <input type="email" placeholder="Password"></input>
+          <input type="password" placeholder="Email"></input>
+          <button className="primary">Login</button>
+          <div className="text-center py-2 text-gray-500">
+            No account?{" "}
+            <Link className="underline text-black" to={"/register"}>
+              {" "}
+              Register here
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
